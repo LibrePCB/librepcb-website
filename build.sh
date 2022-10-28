0a85104d8184c49fdbca3682514948513a234614
U. Bruhin <urbibruhin@bluewin.ch>
2022-10-29 00:54:02 +0200

    Fix relative links on 404.html

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ set -eu -o pipefail
 docker run -t --rm -u `id -u`:`id -g` -v `pwd`:/work -w /work \
     librepcb/librepcb-dev:webtools-1 \
     hugo -F
+
+# Make 404.html working on *any* path
+sed -i 's/"\.\//"\//g' public/404.html
